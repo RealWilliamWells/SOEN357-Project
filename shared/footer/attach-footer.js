57b5ff2612a5889c12b851a6b836d55a949abec7
William Wells <williamwells2013@hotmail.com>
2022-11-21 12:52:15 -0500

    Refactor code and add template page for app pages

---
+++ shared/footer/attach-footer.js
@@ -0,0 +1,14 @@
+$('.footer-parent').append(
+    "<link rel=\"stylesheet\" href=\"../../shared/footer/footer.css\">\n" +
+    "<div class=\"footer\">\n" +
+    "    <div id=\"selected-option\"></div>\n" +
+    "    <img class=\"footer-options-icons\" id=\"home-option-icon\" src=\"../../shared/footer/subtract-1.svg\">\n" +
+    "    <div class=\"footer-options-text\" id=\"home-option-text\">Home</div>\n" +
+    "    <img class=\"footer-options-icons\" id=\"add-option-icon\" src=\"../../shared/footer/subtract-2.svg\">\n" +
+    "    <div class=\"footer-options-text\" id=\"add-option-text\">Add&nbspSub</div>\n" +
+    "    <img class=\"footer-options-icons\" id=\"edit-option-icon\" src=\"../../shared/footer/edit-fill.png\">\n" +
+    "    <div class=\"footer-options-text\" id=\"edit-option-text\">Edit&nbspSub</div>\n" +
+    "    <img class=\"footer-options-icons\" id=\"settings-option-icon\" src=\"../../shared/footer/subtract-3.svg\">\n" +
+    "    <div class=\"footer-options-text\" id=\"settings-option-text\">Settings</div>\n" +
+    "</div>"
+);
