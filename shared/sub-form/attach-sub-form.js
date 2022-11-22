78703de5688ca55396418026b7759d5883bedf46
William Wells <williamwells2013@hotmail.com>
2022-11-22 16:02:22 -0500

    Build most of edit sub page

---
+++ shared/sub-form/attach-sub-form.js
@@ -0,0 +1,80 @@
+$('.sub-form-parent').append(
+    "<link rel=\"stylesheet\" href=\"../../shared/sub-form/sub-form.css\">\n" +
+    "<div class=\"service-name-text\">\n" +
+    "                Service&nbspName\n" +
+    "            </div>\n" +
+    "            <div class=\"service-name-input-parent\">\n" +
+    "                <input type=\"text\" class=\"service-name-input\">\n" +
+    "            </div>\n" +
+    "\n" +
+    "            <div class=\"start-of-cycle-text\">\n" +
+    "                Start&nbspof&nbspCycle\n" +
+    "            </div>\n" +
+    "            <select id=\"start-of-cycle-dropdown-day\" class=\"start-of-cycle-options\">\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"Day-default\">Day</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"1\">1</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"2\">2</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"3\">3</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"4\">4</option>\n" +
+    "            </select>\n" +
+    "            <select id=\"start-of-cycle-dropdown-month\" class=\"start-of-cycle-options\">\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"Day-default\">Month</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"1\">1</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"2\">2</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"3\">3</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"4\">4</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"5\">5</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"6\">6</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"7\">7</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"8\">8</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"9\">9</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"10\">10</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"11\">11</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"12\">12</option>\n" +
+    "            </select>\n" +
+    "            <select id=\"start-of-cycle-dropdown-year\" class=\"start-of-cycle-options\">\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"Day-default\">Year</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"1\">1</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"2\">2</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"3\">3</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"4\">4</option>\n" +
+    "            </select>\n" +
+    "\n" +
+    "            <div class=\"reminder-text\">\n" +
+    "                Reminder\n" +
+    "            </div>\n" +
+    "            <select id=\"reminder-dropdown\" class=\"start-of-cycle-options\">\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"Day-default\">Select Time</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"1\">1</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"2\">2</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"3\">3</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"4\">4</option>\n" +
+    "            </select>\n" +
+    "\n" +
+    "            <div class=\"price-text\">\n" +
+    "                Price&nbsp($)\n" +
+    "            </div>\n" +
+    "            <div class=\"price-input-parent\">\n" +
+    "                <input type=\"text\" class=\"price-input\">\n" +
+    "            </div>\n" +
+    "\n" +
+    "            <div class=\"recurrence-text\">\n" +
+    "                Recurrence&nbspof&nbspSubscription\n" +
+    "            </div>\n" +
+    "            <select id=\"recurrence-dropdown\" class=\"start-of-cycle-options\">\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"Day-default\">Recurrence</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"1\">1</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"2\">2</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"3\">3</option>\n" +
+    "                <option class=\"start-of-cycle-options\" value=\"4\">4</option>\n" +
+    "            </select>\n" +
+    "\n" +
+    "            <button class=\"add-sub-button\">\n" +
+    "            </button>\n" +
+    "\n" +
+    "            <button class=\"cancel-button\">\n" +
+    "            </button>\n" +
+    "            <div class=\"cancel-text\">\n" +
+    "                Cancel\n" +
+    "            </div>"
+);
